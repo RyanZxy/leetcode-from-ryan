5a7ccce0c3eec6e86cde7b8dc6afdea20650443b
RyanZxy <ryandimariazhang@gmail.com>
2021-04-13 21:28:31 +0800

    Update 0001.cpp

--- C++/0001.cpp
+++ C++/0001.cpp
@@ -1,5 +1,41 @@
 /*更新时间：2021-03-29*/
 /*0001.两数之和*/
+/*个人题解*/
+/*执行用时28ms，内存10.1MB*/
+
+class Solution {
+public:
+      vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> temp;
+        unordered_map<int, int> hashmap;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (hashmap.count(nums[i]))  //check if this num is already in map
+			continue;
+		hashmap[nums[i]] = i;
+	}
+	int find;  //find is the expected number for add-up to target
+	for (int i = 0; i < nums.size(); i++)  //mark：！第二次依旧是以数组为基准遍历
+	{
+		find = target - nums[i];
+		auto iter = hashmap.find(find);
+		if (iter != hashmap.end() && iter->second != i)  //the item exist and not 'i'-self
+		{
+            if(i<iter->second){
+            temp.push_back(i);
+            temp.push_back(iter->second);
+			break;
+            }
+            else{
+                temp.push_back(iter->second);
+                temp.push_back(i);
+                break;
+            }
+		}
+	}
+    return temp;
+    }
+};
 
 
 
@@ -80,4 +116,3 @@ public:
     }
 };
 
-//悄悄：这题两年前做的啊哈哈哈看了看自己两年前写的代码简直毫无美观可言哈哈哈哈嗝
